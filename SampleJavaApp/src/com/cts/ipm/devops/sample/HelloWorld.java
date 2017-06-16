13bdceac8918b6cd8ff34fd40d7a80c3cfd75f0a
Mahesh T Venkatramani <mahesh.ramani.bluemix@gmail.com>
2017-06-16 11:50:35 +0530

    First commit

---
+++ SampleJavaApp/src/com/cts/ipm/devops/sample/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.cts.ipm.devops.sample;
+import java.util.Date;
+/**
+ * @author 481535
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date currentDate = new Date();
+		System.out.println("Welcome to HelloWorld Application! Current Date is: "+currentDate);
+	}
+
+}
